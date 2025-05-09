531eb2bc205101ae69d10dc707f4afdd4a57f6aa
Abrar Jahin Ayat <abrarjahinayat123@gmail.com>
2025-05-10 02:56:22 +0600

    routing done

---
+++ pages/Signup.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Signup = () => {
+  return (
+  <div className="flex flex-col justify-center sm:h-screen p-4">
+  <div className="max-w-md w-full mx-auto border border-slate-300 rounded-2xl p-8">
+    <div className="text-center mb-12">
+      <a href="javascript:void(0)">
+        <img
+          src="https://readymadeui.com/readymadeui.svg"
+          alt="logo"
+          className="w-40 inline-block"
+        />
+      </a>
+    </div>
+    <form>
+      <div className="space-y-6">
+        <div>
+          <label className="text-slate-800 text-sm font-medium mb-2 block">
+            Email Id
+          </label>
+          <input
+            name="email"
+            type="text"
+            className="text-slate-800 bg-white border border-slate-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+            placeholder="Enter email"
+          />
+        </div>
+        <div>
+          <label className="text-slate-800 text-sm font-medium mb-2 block">
+            Password
+          </label>
+          <input
+            name="password"
+            type="password"
+            className="text-slate-800 bg-white border border-slate-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+            placeholder="Enter password"
+          />
+        </div>
+        <div>
+          <label className="text-slate-800 text-sm font-medium mb-2 block">
+            Confirm Password
+          </label>
+          <input
+            name="cpassword"
+            type="password"
+            className="text-slate-800 bg-white border border-slate-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+            placeholder="Enter confirm password"
+          />
+        </div>
+        <div className="flex items-center">
+          <input
+            id="remember-me"
+            name="remember-me"
+            type="checkbox"
+            className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-slate-300 rounded"
+          />
+          <label
+            htmlFor="remember-me"
+            className="text-slate-800 ml-3 block text-sm"
+          >
+            I accept the{" "}
+            <a
+              href="javascript:void(0);"
+              className="text-blue-600 font-medium hover:underline ml-1"
+            >
+              Terms and Conditions
+            </a>
+          </label>
+        </div>
+      </div>
+      <div className="mt-12">
+        <button
+          type="button"
+          className="w-full py-3 px-4 text-sm tracking-wider font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer"
+        >
+          Create an account
+        </button>
+      </div>
+      <p className="text-slate-800 text-sm mt-6 text-center">
+        Already have an account?{" "}
+        <Link
+        to={'/login'}
+          href="javascript:void(0);"
+          className="text-blue-600 font-medium hover:underline ml-1"
+        >
+          Login here
+        </Link>
+      </p>
+    </form>
+  </div>
+</div>
+
+  )
+}
+
+export default Signup
---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Home from '../pages/Home';
+import Notfound from '../pages/Notfound';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },  {
+    path: "*",
+    element: <Notfound/>,
+  },{
+    path: "/login",
+    element: <Login/>,
+  },{
+    path: "signup",
+    element: <Signup/>,
+  },
+]);
+const App = () => {
+  return (
+     <RouterProvider router={router} />
+  )
+}
+
+export default App
